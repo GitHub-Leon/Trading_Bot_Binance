0712e2834d66363139a84f3d5a0a92f53351d150
GitHub-Leon <leon.pliessnig@outlook.com>
2021-05-17 11:46:50 +0200

    added sell_all_coins.py
    updated colors.py
    added sell_all_coins.py
    fixed bug with max coins not working properly

--- src/colors.py
+++ src/colors.py
@@ -1,7 +1,9 @@
 # for colors in console output
 
 class txcolors:
-    WIN = '\033[92m'
-    LOSS = '\033[91m'
+    BUY = '\033[92m'
     WARNING = '\033[93m'
+    SELL_LOSS = '\033[91m'
+    SELL_PROFIT = '\033[32m'
+    DIM = '\033[2m\033[35m'
     DEFAULT = '\033[39m'
