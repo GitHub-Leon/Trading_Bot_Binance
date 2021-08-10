1e4eb90c77b5160cffb19184550ee75c4ff9a7c2
FabianWassermann <53076710+FabianWassermann@users.noreply.github.com>
2021-08-10 14:16:43 +0200

    buy/sell trigger | logic

--- src/strategies/elon_mirror/elon_mirror_thread.py
+++ src/strategies/elon_mirror/elon_mirror_thread.py
@@ -4,8 +4,6 @@ import sys
 import threading
 import time
 
-from tradingview_ta import TA_Handler, Interval
-
 from src.config import BTC_BALANCE, FILL_BALANCE, ELON_MIRROR_RECHECK_INTERVAL, bot_paused, DEBUG
 from src.helpers.scripts.logger import debug_log, console_log
 from src.strategies.elon_mirror.check_btc_address import isAction, ActionType, get_action_type
@@ -13,49 +11,55 @@ from src.strategies.elon_mirror.check_btc_address import isAction, ActionType, g
 
 def analyze():
     ignore_sell = False
-    # TODO: get users btc_balance
-    u_btc_balance = 10
+    btc_balance = BTC_BALANCE
+
+    u_btc_balance = 10  # TODO: get users btc_balance
     if BTC_BALANCE > u_btc_balance:  # check if user´s owns less btc than the BTC_BALANCE value
         if FILL_BALANCE:  # only ignore sell triggers if the user wants to fill up first own balance
             ignore_sell = True
+        btc_balance = u_btc_balance
 
     if not isAction(ignore_sell):  # exit analyze if there is no action
-        return
+        return -1, None
 
     # there is action
     action_type = get_action_type()
 
     if action_type == ActionType.BUY:  # Buy action triggered
+        amount_to_buy = 0
+
         debug_log(f'elon_mirror_thread: Buy signal triggered.', False)
         if DEBUG:
             console_log(f'elon_mirror_thread: Buy signal triggered.')
 
-        fill_balance_failed = False
         if FILL_BALANCE:  # let´s try to fill up btc_balance
             u_usdt_balance = 100  # TODO: get amount available on user´s account
             btc_price = 100  # TODO: get current btc price
-            if ((BTC_BALANCE - u_btc_balance) * btc_price) > u_usdt_balance:  # can´t afford that much btc
-                debug_log(f'elon_mirror_thread: Not enough usdt to afford {BTC_BALANCE - u_btc_balance}.', False)
+            if ((btc_balance - u_btc_balance) * btc_price) > u_usdt_balance:  # can´t afford that much btc
+                debug_log(f'elon_mirror_thread: Not enough usdt to afford {btc_balance - u_btc_balance}.', False)
                 if DEBUG:
-                    console_log(f'elon_mirror_thread: Not enough usdt to afford {BTC_BALANCE - u_btc_balance}.')
-                fill_balance_failed = True
+                    console_log(f'elon_mirror_thread: Not enough usdt to afford {btc_balance - u_btc_balance}.')
+
+                amount_to_buy = u_btc_balance / btc_price  # buy as much btc as we can
             else:  # can afford that much btc
-                btc_buy_amount = BTC_BALANCE - u_btc_balance
-                # TODO: Try to place order
+                debug_log(f'elon_mirror_thread: Can afford enough btc to fill up.', False)
+                if DEBUG:
+                    console_log(f'elon_mirror_thread: Can afford enough btc to fill up.')
 
-        if not fill_balance_failed:  # fill balance was successful. end this buy action
-            debug_log(f'elon_mirror_thread: Fill_Balance was successful.', False)
-            if DEBUG:
-                console_log(f'elon_mirror_thread: Fill_Balance was successful.')
-            return
+                amount_to_buy = btc_balance - u_btc_balance  # fill up balance to BTC_BALANCE
 
-        # TODO: buy btc (u_btc_balance * (elon_wallet/elon_buy_amount))
-
+        return amount_to_buy, action_type
     elif action_type == ActionType.SELL:  # Sell action triggered
-        # TODO: Check if we have as much btc as told in BTC_BALANCE
-        # TODO: Sell in percentage (BTC_BALANCE * (elon_wallet/elon_buy_amount))
-        return
-    return
+        amount_to_sell = 0
+
+        debug_log(f'elon_mirror_thread: Sell signal triggered.', False)
+        if DEBUG:
+            console_log(f'elon_mirror_thread: Sell signal triggered.')
+
+        # TODO: Sell in percentage (btc_balance * (elon_wallet/elon_buy_amount))
+        return amount_to_sell, action_type
+
+
 
 
 def do_work():
@@ -68,14 +72,33 @@ def do_work():
             if DEBUG:
                 console_log(f'elon_mirror_thread: Analyzing elon´s btc address')
 
-            analyze()
+            amount, action_type = analyze()
 
-            debug_log(
-                f'elon_mirror_thread: ',
-                False)
-            if DEBUG:
-                console_log(
-                    f'elon_mirror_thread: ')
+            if action_type == ActionType.BUY:
+                debug_log(
+                    f'elon_mirror_thread: Try to buy {amount} btc.',
+                    False)
+                if DEBUG:
+                    console_log(
+                        f'elon_mirror_thread: Try to buy {amount} btc.')
+                # TODO: buy amount btc
+
+            elif action_type == ActionType.SELL:
+                debug_log(
+                    f'elon_mirror_thread: Try to sell {amount} btc.',
+                    False)
+                if DEBUG:
+                    console_log(
+                        f'elon_mirror_thread: Try to sell {amount} btc.')
+                # TODO: sell amount btc
+
+            else:
+                debug_log(
+                    f'elon_mirror_thread: No action to do.',
+                    False)
+                if DEBUG:
+                    console_log(
+                        f'elon_mirror_thread: No action to do.')
 
         except Exception as e:
             debug_log(f"Error in Module: {sys.argv[0]}. Restarting Module", True)
